8afaf6027f7e75cb5e57dea70695386f80867eee
jakepper <epperson.jacob@gmail.com>
2022-10-19 23:40:23 -0600

    tasksheet 1 revisions

---
+++ src/root-finding/Python/functional_iteration.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def func_iter(x0, g, tol, max_iters, verbose=False):
+    if verbose: 
+        print('\n   {:^18s}{:^18s}{:^18s}'.format('Iteration', '\u2248Root', 'Error'))
+    
+    root = np.nan
+    error = 10 * tol
+    i = 0
+    while (error > tol) and (i < max_iters):
+        x1 = g(x0)
+        error = np.abs(x1 - x0)
+        x0 = x1
+        root = x0
+        if verbose: 
+            print('   {:^18d}{:^18.4e}{:^18e}'.format(i + 1, x0, error))
+        i += 1
+    
+    print('\n   Approximate Root: {:f}'.format(root))
+    return root
---
+++ src/root-finding/Python/tasksheet1.py
@@ -0,0 +1,83 @@
+import sys
+import argparse
+import warnings
+import numpy as np
+
+from functional_iteration import func_iter
+from bisection import bisect
+
+TOLERANCE = 0.000001
+MAX_ITERS = 25
+
+warnings.filterwarnings("ignore")
+
+def test_task_1(verbose):
+    print('\nFunctional Iteration with f(x) = x * e^(-x)')
+    x0 = 1.23456
+    print('\n   g -> x - f(x)')
+    g = lambda x: x - x * np.exp(-x)
+    func_iter(x0, g, TOLERANCE, MAX_ITERS, verbose=verbose)
+    g = lambda x: x + x * np.exp(-x)
+    print('\n   g -> x + f(x)')
+    func_iter(x0, g, TOLERANCE, MAX_ITERS, verbose=verbose)
+    print()  
+
+def test_task_3(verbose):
+    print('\nFunctional Iteration with f(x) = 10.14 * e^(x^2) * cos(\u03C0/x)')
+    n = 20
+    x0 = -3.0
+    dx = 10.0 / n
+    g = lambda x: x - 10.14 * (np.exp(x*x)) * np.cos(np.pi / x)
+    # without epsilon
+    print('\n   g -> x - f(x)')
+    for x in range(n):
+        if x0 != 0:
+            func_iter(x0, g, TOLERANCE, MAX_ITERS, verbose=verbose)
+        x0 += dx
+    # with epsilon = 0.000001
+    x0 = 3.0
+    epsilon = 0.000001
+    g = lambda x: x - epsilon * (10.14 * (np.exp(x*x)) * np.cos(np.pi / x))
+    print(f'\n   g -> x - \u03B5f(x), \u03B5 = {epsilon}')
+    for x in range(n):
+        if x0 != 0:
+            func_iter(x0, g, TOLERANCE, MAX_ITERS, verbose=verbose)
+        x0 += dx
+    print()
+    
+def test_task_4(verbose=False):
+    print('\nBisection with f(x) = x * e^(-x)')
+    a = -1.234
+    b = 2.654
+    f = lambda x: x * np.exp(-x)
+    bisect(a, b, f, TOLERANCE, verbose=verbose)
+    print('\nBisection with f(x) = 10.14 * e^(x^2) * cos(\u03C0/x)')
+    a = 0.6
+    b = 0.7
+    f = lambda x: 10.14 * (np.exp(x*x)) * np.cos(np.pi / x)
+    bisect(a, b, f, TOLERANCE, verbose=verbose)
+
+FUNCTIONS = {
+    '1': test_task_1,
+    '2': test_task_1,
+    '3': test_task_3,
+    '4': test_task_4,
+}
+
+parser = argparse.ArgumentParser(description='Run test functions on the following')
+parser.add_argument('-t', '--tasks', 
+                        nargs='+', 
+                        choices=FUNCTIONS, 
+                        help='Tasks to be tested',
+                        required=True
+                    )
+parser.add_argument('-v', '--verbose', 
+                        action='store_true', 
+                        default=False,
+                        help="Test Task 1" 
+                    )
+
+args = parser.parse_args(sys.argv[1:])
+
+for test in args.tasks:
+    FUNCTIONS[test](args.verbose)
