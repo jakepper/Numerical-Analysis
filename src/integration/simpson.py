24498d5192fd197ff3409448d832e88c1392b15a
jakepp <jakepp@aggiemail.usu.edu>
2022-11-07 14:56:55 -0700

    simpson modification

--- src/integration/simpson.py
+++ src/integration/simpson.py
@@ -4,7 +4,7 @@ def simpson(f, a, b, n):
     h = (b - a) / n
     X = np.linspace(a, b, n)
     Y = np.array([f(x) for x in X])
-    sum1 = 2 * sum(Y[:n-2:2])
+    sum1 = 2 * sum(Y[2:n-2:2])
     sum2 = 4 * sum(Y[1:n-1:2])
 
     return (h / 3) * (f(a) + sum1 + sum2 + f(b))
